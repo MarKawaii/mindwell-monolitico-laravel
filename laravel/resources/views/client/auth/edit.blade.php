100d7ca5c3c3a8afba6dbc7b0e756dd4ed193f3d
Ignacio Veliz <101842633+MarKawaii@users.noreply.github.com>
2023-11-19 01:11:12 -0300

    Update edit.blade.php

--- laravel/resources/views/client/auth/edit.blade.php
+++ laravel/resources/views/client/auth/edit.blade.php
@@ -4,11 +4,86 @@
 @endsection
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div>
-                <h1>Hola mundo</h1>
+    <div class="container mt-5 mb-5">
+
+        <div class="row justify-content-center">
+            <div class="col-md-6 mb-5 mb-md-0">
+                <h2 class="h3 mb-3 text-black">Modificar Datos</h2>
+                <div class="p-3 p-lg-5 border bg-white">
+                    <div class="form-group row">
+                        <div class="col-md-12">
+                            <label for="c_fname" class="text-black">Nombre Completo <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="c_fname" name="c_fname">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="col-md-12">
+                            <label for="c_companyname" class="text-black">Correo </label>
+                            <input type="text" class="form-control" id="c_companyname" name="c_companyname">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="col-md-12">
+                            <label for="c_address" class="text-black">Address <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="c_address" name="c_address"
+                                placeholder="Street address">
+                        </div>
+                    </div>
+
+                    <div class="form-group mt-3">
+                        <input type="text" class="form-control" placeholder="Apartment, suite, unit etc. (optional)">
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="col-md-6">
+                            <label for="c_state_country" class="text-black">State / Country <span
+                                    class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="c_state_country" name="c_state_country">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="c_postal_zip" class="text-black">Posta / Zip <span
+                                    class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="c_postal_zip" name="c_postal_zip">
+                        </div>
+                    </div>
+
+                    <div class="form-group row mb-5">
+                        <div class="col-md-6">
+                            <label for="c_email_address" class="text-black">Email Address <span
+                                    class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="c_email_address" name="c_email_address">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="c_phone" class="text-black">Phone <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="c_phone" name="c_phone"
+                                placeholder="Phone Number">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="c_create_account" class="text-black" data-bs-toggle="collapse" href="#create_an_account"
+                            role="button" aria-expanded="false" aria-controls="create_an_account"><input type="checkbox"
+                                value="1" id="c_create_account"> Create an account?</label>
+                        <div class="collapse" id="create_an_account">
+                            <div class="py-2 mb-4">
+                                <p class="mb-3">Create an account by entering the information below. If you are a
+                                    returning customer please login at the top of the page.</p>
+                                <div class="form-group">
+                                    <label for="c_account_password" class="text-black">Account Password</label>
+                                    <input type="email" class="form-control" id="c_account_password"
+                                        name="c_account_password" placeholder="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+
+
         </div>
+        <!-- </form> -->
     </div>
 @endsection
