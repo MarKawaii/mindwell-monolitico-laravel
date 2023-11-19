66f4f55e995304436377e1e6ca9c6d0a8db9ed15
Ignacio Veliz <101842633+MarKawaii@users.noreply.github.com>
2023-11-19 01:50:24 -0300

    Update edit.blade.php

--- laravel/resources/views/client/auth/edit.blade.php
+++ laravel/resources/views/client/auth/edit.blade.php
@@ -27,9 +27,46 @@
                     <div class="form-group row">
                         <div class="col-md-12">
                             <label for="direccion" class="text-black">Dirección <span class="text-danger">*</span></label>
-                            <input type="text" class="form-control" name="direccion" placeholder="Direccion 1212, Maipu" required>
+                            <input type="text" class="form-control" name="direccion" placeholder="Direccion 1212, Maipu"
+                                required>
                         </div>
                     </div>
+
+                    <div class="form-group row">
+                        <div class="col-md-6">
+                            <label for="ciudad" class="text-black">Ciudad</label>
+                            <select class="form-control" id="ciudad" name="ciudad" onchange="updateComunas()" required>
+                                <option value="">Seleccione una ciudad</option>
+                                <option value="arica">Arica</option>
+                                <option value="iquique">Iquique</option>
+                                <option value="antofagasta">Antofagasta</option>
+                                <option value="copiapo">Copiapó</option>
+                                <option value="la_serena">La Serena</option>
+                                <option value="coquimbo">Coquimbo</option>
+                                <option value="valparaiso">Valparaíso</option>
+                                <option value="viña_del_mar">Viña del Mar</option>
+                                <option value="rancagua">Rancagua</option>
+                                <option value="talca">Talca</option>
+                                <option value="concepcion">Concepción</option>
+                                <option value="temuco">Temuco</option>
+                                <option value="valdivia">Valdivia</option>
+                                <option value="puerto_montt">Puerto Montt</option>
+                                <option value="coyhaique">Coyhaique</option>
+                                <option value="punta_arenas">Punta Arenas</option>
+                                <option value="santiago">Santiago</option>
+                            </select>
+
+                        </div>
+                        <div class="col-md-6">
+                            <label for="comuna" class="text-black">Comuna <span class="text-danger">*</span></label>
+                            <select class="form-control" id="comuna" name="comuna" required>
+                                <option value="">Seleccione una comuna</option>
+                                <!-- Añade aquí las opciones de comuna -->
+                            </select>
+                        </div>
+                    </div>
+
+
 
                     <div class="form-group row">
                         <div class="col-md-6">
@@ -45,11 +82,13 @@
                     <div class="form-group row mb-5">
                         <div class="col-md-6">
                             <label for="celular" class="text-black">Celular <span class="text-danger">*</span></label>
-                            <input type="text" class="form-control" name="celular" placeholder="99999999" value="{{ $user['celular'] }}" required>
+                            <input type="text" class="form-control" name="celular" placeholder="99999999"
+                                value="{{ $user['celular'] }}" required>
                         </div>
                         <div class="col-md-6">
                             <label for="telefono" class="text-black">Telefono Fijo</label>
-                            <input type="text" class="form-control" name="telefono" placeholder="99999999" value="{{ $user['telefono'] }}">
+                            <input type="text" class="form-control" name="telefono" placeholder="99999999"
+                                value="{{ $user['telefono'] }}">
                         </div>
                     </div>
 
@@ -62,6 +101,61 @@
 
 
         </div>
-        <!-- </form> -->
     </div>
+
+    
+
+    <script>
+        function updateComunas() {
+            var comunasPorCiudad = {
+                arica: ["Arica", "Camarones", "Putre"],
+                iquique: ["Iquique", "Alto Hospicio", "Pica"],
+                antofagasta: ["Antofagasta", "Calama", "Tocopilla"],
+                copiapo: ["Copiapó", "Caldera", "Vallenar"],
+                la_serena: ["La Serena", "Coquimbo", "Vicuña"],
+                coquimbo: ["Coquimbo", "La Serena", "Ovalle"],
+                valparaiso: ["Valparaíso", "Viña del Mar", "Quilpué"],
+                viña_del_mar: ["Viña del Mar", "Valparaíso", "Concón"],
+                rancagua: ["Rancagua", "Machalí", "Graneros"],
+                talca: ["Talca", "Maule", "Linares"],
+                concepcion: ["Concepción", "Talcahuano", "Chiguayante"],
+                temuco: ["Temuco", "Padre las Casas", "Villarrica"],
+                valdivia: ["Valdivia", "La Unión", "Paillaco"],
+                puerto_montt: ["Puerto Montt", "Puerto Varas", "Calbuco"],
+                coyhaique: ["Coyhaique", "Aysén", "Cisnes"],
+                punta_arenas: ["Punta Arenas", "Puerto Natales", "Porvenir"],
+                santiago: [
+                    "Cerrillos", "Cerro Navia", "Conchalí", "El Bosque",
+                    "Estación Central", "Huechuraba", "Independencia", "La Cisterna",
+                    "La Florida", "La Granja", "La Pintana", "La Reina",
+                    "Las Condes", "Lo Barnechea", "Lo Espejo", "Lo Prado",
+                    "Macul", "Maipú", "Ñuñoa", "Pedro Aguirre Cerda", "Peñalolén",
+                    "Providencia", "Pudahuel", "Quilicura", "Quinta Normal", "Recoleta",
+                    "Renca", "San Joaquín", "San Miguel", "San Ramón", "Santiago",
+                    "Vitacura"
+                ],
+            };
+
+            var ciudadSeleccionada = document.getElementById("ciudad").value;
+            var comunas = comunasPorCiudad[ciudadSeleccionada] || [];
+            var comunaSelect = document.getElementById("comuna");
+
+            // Limpiar las opciones anteriores
+            comunaSelect.innerHTML = '';
+
+            // Opción por defecto
+            var option = document.createElement("option");
+            option.value = "";
+            option.text = "Seleccione una comuna";
+            comunaSelect.appendChild(option);
+
+            // Añadir nuevas opciones
+            comunas.forEach(function(comuna) {
+                var option = document.createElement("option");
+                option.value = comuna.toLowerCase().replace(/\s+/g, '_');
+                option.text = comuna;
+                comunaSelect.appendChild(option);
+            });
+        }
+    </script>
 @endsection
